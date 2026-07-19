067d4d1ca3dbb554afa3890ce334321a63f2c1ae
Leulseged-Mesfin <leulsegedmesfin16@gmail.com>
2026-07-19 20:41:37 +0300

    added whitenoise middleware.

--- multi_inventory_check/settings.py
+++ multi_inventory_check/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django_tenants.middleware.main.TenantMainMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
