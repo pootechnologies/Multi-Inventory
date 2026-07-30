d1cea5735a431c5c88441b36d25b96edac114040
Po'o Technologies <pootechnologies1@gmail.com>
2026-07-30 11:43:54 +0300

    Implement image resizing and format conversion in save
    Added image processing in save method to downsize and convert images to WEBP format.

--- inventory/models.py
+++ inventory/models.py
@@ -8,6 +8,10 @@ from django.core.exceptions import ValidationError
 from django.db.models import Sum
 from decimal import Decimal
 from django.db import transaction
+import sys
+from io import BytesIO
+from PIL import Image
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
 
@@ -70,6 +74,27 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        if self.image:
+            img = Image.open(self.image)
+            
+            # --- THIS IS THE MISSING PIECE ---
+            # It downsizes the massive pixel grid to a web-friendly size
+            max_dimensions = (1200, 1200)
+            img.thumbnail(max_dimensions, Image.Resampling.LANCZOS)
+            # ----------------------------------
+
+            output_io = BytesIO()
+            img.save(output_io, format='WEBP', quality=70) # Or 'JPEG'
+            output_io.seek(0)
+            
+            base_name = self.image.name.split('.')[0]
+            self.image = InMemoryUploadedFile(
+                output_io, 'ImageField', f"{base_name}.webp", 'image/webp', sys.getsizeof(output_io), None
+            )
+            
+        super().save(*args, **kwargs)
+
 class Bundle(models.Model):
     bundle = models.ForeignKey(Product, related_name='bundle_components', on_delete=models.CASCADE)
     
@@ -124,6 +149,27 @@ class CompanyInfo(models.Model):
 
     def __str__(self):
         return self.en_name
+
+    def save(self, *args, **kwargs):
+        if self.image:
+            img = Image.open(self.logo)
+            
+            # --- THIS IS THE MISSING PIECE ---
+            # It downsizes the massive pixel grid to a web-friendly size
+            max_dimensions = (1200, 1200)
+            img.thumbnail(max_dimensions, Image.Resampling.LANCZOS)
+            # ----------------------------------
+
+            output_io = BytesIO()
+            img.save(output_io, format='WEBP', quality=70) # Or 'JPEG'
+            output_io.seek(0)
+            
+            base_name = self.image.name.split('.')[0]
+            self.image = InMemoryUploadedFile(
+                output_io, 'ImageField', f"{base_name}.webp", 'image/webp', sys.getsizeof(output_io), None
+            )
+            
+        super().save(*args, **kwargs)
 
 class Order(models.Model):
     ACTION_CHOICES = [
